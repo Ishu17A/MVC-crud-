a955e1b3e058a0d1ea103f46575b6b366afcfe0f
ishan <ishan@DESKTOP-TOATOCU>
2023-04-15 18:10:54 +0530

    Add project files.

---
+++ CodeFirstMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=MyConnectionString")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
